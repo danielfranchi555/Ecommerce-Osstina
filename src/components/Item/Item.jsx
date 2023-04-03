b53f732b4ef5f7ae162c2da8c5b0e4c2f6fccd6d
danielfranchi <danielfranchi3@gmail.com>
2023-04-03 01:00:26 -0300

    commit

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -8,9 +8,7 @@ const Item = ({item}) => {
 
     
        <Stack justify='center' spacing={0} p={1} align='center'  w='auto'> 
-       <Fade>
          <Image src={item.image} w={{base:'auto',md:'auto'}} alt="" />
-       </Fade>    
                <Text>Buzo</Text>
                <span style={{fontWeight:'800'}}> ${item.price}</span>
               </Stack>
