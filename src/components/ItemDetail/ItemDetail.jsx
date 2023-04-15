2137fe796648d3d1f6fd898aaa9e42610a81dc23
danielfranchi <danielfranchi3@gmail.com>
2023-04-15 15:55:28 -0300

    commit

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,55 +1,49 @@
 import { Container, Image, Stack, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
-export const ItemDetail = ({ product}) => {
+export const ItemDetail = ({ product }) => {
+  const fuckingImage = product.image ? product.image : "no";
 
+  const [imageCarousel, setimageCarousel] = useState(fuckingImage);
 
-   
-
-  
-   const fuckingImage = product.image?product.image:'no'
-
-  const [imageCarousel,setimageCarousel]=useState(fuckingImage)
- 
-
-
-
- console.log(fuckingImage)
-
+  console.log(fuckingImage);
 
   return (
-    <Container maxW='container.lg'>
-           <Stack mt='50px' justify="center" align="center">
-      <Stack direction={['column', 'row']}>
-        <Stack   flex={6}>
-          <Image
-          objectFit='cover'
-          boxSize={{base:'auto',md:'400px'}}
-          src={imageCarousel} alt="" /> 
-          <Stack   direction='row'>
-             {product.carousel? product.carousel.map((item)=>(
-        <Stack key={item} >
-            <Image         
-            onClick={()=>setimageCarousel(item)}
-            boxSize={{base:'auto',md:'70px'}}
-            objectFit="cover"
-            src={item}
-          />
-                </Stack>
-            ))
-            :
-            null
-        
-        } 
+    <Container  h="auto" w="100%" maxW="container.lg">
+      <Stack mt="20px" justify="center" align="center">
+        <Stack w={{ base: "auto", md: "700px" }} direction={["column", "row"]}>
+          <Stack direction={["column", "column"]} flex={7}>
+            <Image
+             boxShadow='lg' rounded='lg' 
+              boxSize={{ base: "100%", md: "500px" }}
+              objectFit="cover"
+              src={imageCarousel}
+              alt="Dan Abramov"
+            />
+            <Stack direction="row">
+              {product.carousel
+                ? product.carousel.map((item) => (
+                    <Stack w="100%" key={item}>
+                      <Image
+                        borderRadius="lg"
+                        cursor="pointer"
+                        onClick={() => setimageCarousel(item)}
+                        h={{ base: "100px", md: "110px" }}
+                        w="100%"
+                        objectFit="cover"
+                        src={item}
+                      />
+                    </Stack>
+                  ))
+                : null}
+            </Stack>
+          </Stack>
+          <Stack spacing={0} flex={6}>
+            <Text fontSize="30px">{product.title}</Text>
+            <Text fontSize="20px">${product.price}</Text>
           </Stack>
         </Stack>
-        <Stack justify='center' align='center' spacing={0} flex={6}>
-          <Text fontSize='30px'>{product.title}</Text>
-          <Text fontSize='20px'>${product.price}</Text>
-        </Stack>
       </Stack>
-    </Stack>
     </Container>
- 
   );
 };
