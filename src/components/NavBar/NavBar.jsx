464272b8fcc5ea2324770894df9cec1f76863690
danielfranchi <danielfranchi3@gmail.com>
2023-04-16 02:51:44 -0300

    commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,15 +5,15 @@ const NavBar = () => {
     <>
       <Stack bg=" #f9f9f9">
         <Container maxWidth="container.xl">
-          <Stack direction="row" alignItems="center">
-            <Stack h="auto" flex={1}>
-              <Input w={{ base: "100px", md: "150px" }} placeholder="buscar" />
+          <Stack h='70px'  direction="row" alignItems="center">
+            <Stack  h="auto" flex={1}>
+              <Input w={{ base: "100px", md: "100px" }}  placeholder="buscar" />
             </Stack>
-            <Text textAlign="center" flex={1} fontSize="40px">
+            <Text textAlign="center" flex={1} fontSize="45px">
               <a href="/">Osstina</a>
             </Text>
 
-            <Stack align="flex-end" flex={1}>
+            <Stack  align="center" flex={1}>
               <img src={carrito} style={{ width: "30px" }} alt="" />
             </Stack>
           </Stack>
